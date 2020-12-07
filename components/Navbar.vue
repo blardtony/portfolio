0a4444543408331d8ea75c77a88d600c80eb072d
Tony <tonyblard55@gmail.com>
2020-12-07 18:24:05 +0100

    Création des différents composants + Nav bar

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <b-navbar toggleable="true" type="dark" variant="dark">
+            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="#">Link</b-nav-item>
+                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
