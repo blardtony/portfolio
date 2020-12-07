5a770d52455b3b31d4bd74b003cf73578d6f8497
Tony <tonyblard55@gmail.com>
2020-12-08 00:10:49 +0100

    Navbar done + home page in progress

---
+++ components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <b-row 
+            class="vh-100"
+            align-v="center"
+        >
+            <b-col>
+                <h1>Bonjour, je suis Tony Blard</h1>
+                <h2>Développeur Web Full Stack</h2>
+            </b-col>
+            <b-col>
+                <h1>Image à définir</h1>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body {
+    overflow: hidden;
+}
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,19 +1,36 @@
 <template>
     <div>
-        <b-navbar toggleable="true" type="dark" variant="dark">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar toggleable="md" type="dark" variant="dark">
+            <b-navbar-brand href="#">Logo</b-navbar-brand>
 
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item href="#">Link</b-nav-item>
-                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            <b-collapse id="nav-collapse"  is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item href="#">Accueil</b-nav-item>
+                    <b-nav-item href="#">À propos</b-nav-item>
+                    <b-nav-item href="#">Compétences</b-nav-item>
+                    <b-nav-item href="#">Mes projet</b-nav-item>
+                    <b-nav-item href="#">Contact</b-nav-item>
+                    
                 </b-navbar-nav>
 
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
-
+                    <b-nav-item 
+                        rel="noopener noreferrer"
+                        target="_blank" 
+                        href="https://www.linkedin.com/in/tony-blard-1483a1168"
+                    >   
+                        <font-awesome-icon :icon="['fab', 'linkedin']"/>
+                    </b-nav-item>
+                    <b-nav-item 
+                        target="_blank" 
+                        href="https://github.com/blardtony"
+                        rel="noopener noreferrer"
+                    >   
+                        <font-awesome-icon :icon="['fab', 'github']"/>
+                    </b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
