c5486458c51776b9ba19855b19a873d70a785ba8
Tony <tonyblard55@gmail.com>
2020-12-08 21:34:54 +0100

    Home page Affichage repsonsive

--- components/Home.vue
+++ components/Home.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="home">
         <b-row 
-            class="vh-100"
+            class="vh-100 text-center text-lg-left"
             align-v="center"
+
+            
         >
-            <b-col>
+            <b-col
+                cols="12"
+                lg="6"
+            >
                 <h1>Bonjour, je suis Tony Blard</h1>
-                <h2>Développeur Web Full Stack</h2>
+                <h3 class="">Développeur Web Full Stack</h3>
             </b-col>
-            <b-col>
+            <b-col
+                cols="12"
+                lg="6"
+            >
                 <h1>Image à définir</h1>
             </b-col>
         </b-row>
